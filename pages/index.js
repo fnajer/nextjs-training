e16df5c8697cc04fbcad5bbfe6c622649d8455c4
fnajer <lexactr@gmail.com>
2020-01-21 02:28:21 +0300

    link two pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/Link';
+
 const Index = () => (
   <div>
     <p>Hello Next.js</p>
+    <Link href="/about">
+      <a title="About Page">About Page</a>
+    </Link>
   </div>
 );
 
